935a56057de0c0c0c0aeb24adf191101a56dd50e
wernermatthias92-cloud <werner.matthias.92@gmail.com>
2026-04-21 15:24:19 +0200

    Update parallel_drossel.py

--- system/parallel_drossel.py
+++ system/parallel_drossel.py
@@ -1,16 +1,22 @@
 import math
-from membrane.modell import berechne_tcf, berechne_a_wert
 from hydraulik.widerstand import berechne_hydraulischen_widerstand
 
-# Hilfsfunktion, um den Druckabfall durch eine feste Drossel zu berechnen
 def berechne_drossel_druckabfall(flow_lh, drossel_mm):
-    if flow_lh <= 0 or drossel_mm <= 0: return 1e6
+    # Verhindert Division durch 0 und fängt negative Flüsse ab
+    if flow_lh <= 0.1 or drossel_mm <= 0: return 1e6 
+    
     q_ms = (flow_lh / 1000) / 3600
     d_m = drossel_mm / 1000.0
     area_m2 = math.pi * (d_m / 2)**2
-    v_spalt = q_ms / area_m2
-    delta_p_pa = (v_spalt**2 * 2.5 * 1000) / 2
-    return delta_p_pa / 100000.0
+    
+    # Reale physikalische Blenden-Gleichung (Orifice Equation)
+    # Ein typisches Nadelventil / Drossel hat einen Ausflusskoeffizienten (Cd) von ca. 0.6
+    c_d = 0.6
+    v_spalt = q_ms / (area_m2 * c_d)
+    
+    # Delta P = (rho * v^2) / 2
+    delta_p_pa = (1000 * v_spalt**2) / 2
+    return delta_p_pa / 100000.0 # Umrechnung in bar
 
 def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm, m_flaeche, m_test_flow,
                        m_test_druck, m_rueckhalt, tds_feed, temp, p_system,
@@ -18,9 +24,14 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
                        leitungen_konz, leitung_out,
                        p_leitungen_konz, p_leitung_out, p_schlauch_out):
     
-    tcf = berechne_tcf(temp)
-    a_wert = berechne_a_wert(m_test_flow, m_flaeche, m_test_druck)
     anzahl_membranen = len(flow_fractions)
+
+    # 1. Wissenschaftliches TCF (Temperatur-Korrektur-Faktor)
+    # Basis: 25°C (298.15 K). Aktivierungsenergie-Konstante = 3020 K.
+    tcf_real = math.exp(3020 * (1/298.15 - 1/(273.15 + temp)))
+    
+    # 2. Reiner A-Wert der Membran bei Testbedingungen (25°C, also TCF=1.0)
+    a_wert = m_test_flow / (m_flaeche * m_test_druck * 1.0 * 1000)
 
     # Vorab-Berechnungen der Leitungs-Widerstände
     r_p_out = berechne_hydraulischen_widerstand(p_leitung_out['d'], p_leitung_out['l'], [], p_leitung_out['b'])
@@ -33,13 +44,11 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
             r_p_branches.append(berechne_hydraulischen_widerstand(p_cfg['d'], p_cfg['l'], [], p_cfg['b']))
             
     # --- NUMERISCHER SOLVER START ---
-    # Suchbereich für den Feed-Strom (10 l/h bis extrem hohe 15.000 l/h)
     feed_min = 10.0
     feed_max = 15000.0
     best_result = None
 
-    # Bisektionsverfahren (40 Iterationen reichen für nahezu 100% Präzision)
-    for iteration in range(40):
+    for iteration in range(50):
         q_feed_start_lh = (feed_min + feed_max) / 2
         
         q_ms = (q_feed_start_lh / 1000) / 3600
@@ -48,7 +57,6 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
         p_verlust_netzwerk = (r_netzwerk * q_ms**2) / 100000 if hat_t_stueck else 0
         p_effektiv_start = p_system - p_verlust_druck_haupt - p_verlust_netzwerk
         
-        # Falls der Feed so hoch geraten wurde, dass der Druck negativ wird
         if p_effektiv_start <= 0:
             feed_max = q_feed_start_lh
             continue
@@ -57,7 +65,7 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
         total_permeat_salzfracht = 0
         membran_daten = []
         
-        # Schätzung für den Haupt-Gegendruck
+        # Schätzung Haupt-Gegendruck Permeat
         q_p_approx_total = q_feed_start_lh * 0.4
         q_ms_p = (q_p_approx_total / 1000) / 3600
         p_back_main = ((r_p_out + r_p_schlauch) * q_ms_p**2) / 100000 + p_back_height
@@ -69,15 +77,16 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
             q_p = (f_in / anzahl_membranen) * 0.5 
             tds_p = tds_feed * (1 - m_rueckhalt)
             
-            # Physik-Schleife für die Membranen
-            for _ in range(10):
-                if q_p > f_in * 0.90: q_p = f_in * 0.90 
+            # Physik-Schleife Membran
+            for _ in range(15):
+                # Verhindert Div/0 bei Berechnungen der Konzentration
+                if q_p >= f_in: q_p = f_in * 0.99 
                 if q_p < 0: q_p = 0
                 
                 q_c_temp = f_in - q_p
                 recovery = q_p / f_in if f_in > 0 else 0
                 
-                tds_c_temp = ((f_in * tds_feed) - (q_p * tds_p)) / q_c_temp if q_c_temp > 0 else tds_feed
+                tds_c_temp = ((f_in * tds_feed) - (q_p * tds_p)) / q_c_temp
                 tds_avg = (tds_feed + tds_c_temp) / 2
                 cp_factor = math.exp(0.7 * recovery) 
                 tds_wall = tds_avg * cp_factor
@@ -87,11 +96,18 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
                 p_back_branch = (r_p_branch * ((q_p / 1000) / 3600)**2) / 100000
                 p_back_total = p_back_main + p_back_branch
                 
-                pi_wall = (tds_wall / 100) * 0.07
-                ndp = max(0, p_effektiv_start - pi_wall - p_back_total)
+                # Druckabfall IM Modul durch Spacer (ca. 0.2 bar bei 1000 l/h)
+                p_verlust_modul = 0.2 * (f_in / 1000)**1.5
+                p_effektiv_mitte = p_effektiv_start - (p_verlust_modul / 2)
                 
-                q_p_target = m_flaeche * a_wert * ndp * tcf * 1000
+                pi_wall = (tds_wall / 100) * 0.07
+                ndp = max(0, p_effektiv_mitte - pi_wall - p_back_total)
+                
+                q_p_target = m_flaeche * a_wert * ndp * tcf_real * 1000
                 q_p = q_p * 0.5 + q_p_target * 0.5
+                
+                # BUGFIX: Harte Sicherheitsgrenze AM ENDE der Iteration
+                if q_p > f_in * 0.95: q_p = f_in * 0.95
                 
             q_c = f_in - q_p
             tds_c = ((f_in * tds_feed) - (q_p * tds_p)) / q_c if q_c > 0 else tds_feed
@@ -124,19 +140,15 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
             r_out = berechne_hydraulischen_widerstand(leitung_out['d'], leitung_out['l'], [], leitung_out['b'])
             p_vor_ventil = p_t_stueck - (r_out * ((end_konzentrat_flow/1000)/3600)**2 / 100000)
 
-        # --- SOLVER LOGIK: Passt die Drossel zum Strom? ---
+        # --- SOLVER LOGIK ---
         p_verlust_drossel = berechne_drossel_druckabfall(end_konzentrat_flow, drossel_vorgabe_mm)
         restdruck_nach_ventil = p_vor_ventil - p_verlust_drossel
 
-        # Ziel ist es, nach der Drossel ca. 0.5 bar Überdruck übrig zu haben (wie in der anderen Simulation)
-        if restdruck_nach_ventil > 0.5:
-            # Das Ventil bremst zu wenig. Es muss MEHR Wasser fließen, um den Rohr- & Ventil-Widerstand zu erhöhen
+        if restdruck_nach_ventil > 0.1:
             feed_min = q_feed_start_lh
         else:
-            # Das Ventil bremst zu stark. Es muss WENIGER Wasser fließen.
             feed_max = q_feed_start_lh
 
-        # Resultate speichern
         avg_permeat_tds = total_permeat_salzfracht / total_permeat if total_permeat > 0 else 0
         final_konzentrat_tds = (q_feed_start_lh * tds_feed - total_permeat_salzfracht) / end_konzentrat_flow if end_konzentrat_flow > 0 else tds_feed
 
@@ -154,7 +166,7 @@ def simuliere_parallel_drossel(flow_fractions, membran_namen, drossel_vorgabe_mm
             "p_effektiv_start": p_effektiv_start,
             "konzentrat_druck_verlauf": p_vor_ventil,
             "abzubauender_druck": p_verlust_drossel,
-            "empfohlene_drossel_mm": drossel_vorgabe_mm # Die Eingabe ist gleichzeitig das "Ergebnis"
+            "empfohlene_drossel_mm": drossel_vorgabe_mm 
         }
 
     return best_result
