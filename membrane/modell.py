c84bed268bd125bd3165cf573b8b05c4f497fc34
wernermatthias92-cloud <werner.matthias.92@gmail.com>
2026-04-21 09:49:45 +0200

    Update modell.py

--- membrane/modell.py
+++ membrane/modell.py
@@ -1,1 +1,11 @@
+import math
 
+def berechne_tcf(temp_c):
+    """Berechnet den Temperaturkorrekturfaktor (TCF)."""
+    return math.exp(2640 * (1/298.15 - 1/(temp_c + 273.15)))
+
+def berechne_a_wert(m_test_flow, m_flaeche, m_test_druck):
+    """Berechnet die Permeabilität (A-Wert) der Membran."""
+    # 1500 ppm NaCl entsprechen grob 1.05 bar osmotischem Druck
+    pi_test = (1500 / 100) * 0.07 
+    return (m_test_flow / 1000) / (m_flaeche * (m_test_druck - pi_test))
