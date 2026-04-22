d5cc48aa8f273717e5fcf1897629ca030f26a446
wernermatthias92-cloud <werner.matthias.92@gmail.com>
2026-04-22 13:49:36 +0200

    Refactor parallel_drossel function for efficiency

--- system/parallel_drossel.py
+++ system/parallel_drossel.py
@@ -14,39 +14,21 @@ def simuliere_parallel_drossel(
     L_out,
     L_drossel
 ):
-
     p_feed = p_feed_bar * 1e5
     p_perm = p_perm_bar * 1e5
 
-    membranes = []
-    k_in_list = []
-    k_out_list = []
-    k_drossel_list = []
+    membranes = [Membrane(A, area, tds) for _ in range(n_membranes)]
 
-    for _ in range(n_membranes):
+    k_in = [pipe_k(d_in, L_in) for _ in range(n_membranes)]
+    k_out = [pipe_k(d_out, L_out) for _ in range(n_membranes)]
+    k_drossel = [pipe_k(d_drossel, L_drossel, zeta=2.0) for _ in range(n_membranes)]
 
-        membranes.append(Membrane(A, area, tds))
-
-        k_in_list.append(pipe_k(d_in, L_in))
-        k_out_list.append(pipe_k(d_out, L_out))
-
-        k_drossel_list.append(
-            pipe_k(d_drossel, L_drossel, zeta=2.0)
-        )
-
-    result = solve_parallel_system(
-        n=n_membranes,
-        p_feed=p_feed,
-        p_perm=p_perm,
-        membranes=membranes,
-        k_in_list=k_in_list,
-        k_out_list=k_out_list,
-        k_drossel_list=k_drossel_list
+    return solve_parallel(
+        n_membranes,
+        p_feed,
+        p_perm,
+        membranes,
+        k_in,
+        k_out,
+        k_drossel=k_drossel
     )
-
-    return result
-
-
-# optional alias
-def berechne_parallel_drossel(*args, **kwargs):
-    return simuliere_parallel_drossel(*args, **kwargs)
