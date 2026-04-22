b5966b07429b22b2dd48583cbb7fff2c801bb7bd
wernermatthias92-cloud <werner.matthias.92@gmail.com>
2026-04-22 13:42:34 +0200

    Update parallel.py

--- system/parallel.py
+++ system/parallel.py
@@ -13,6 +13,7 @@ def berechne_parallel(
     L_out,
     target_recovery
 ):
+
     p_feed = p_feed_bar * 1e5
     p_perm = p_perm_bar * 1e5
 
@@ -20,14 +21,12 @@ def berechne_parallel(
     k_in_list = []
     k_out_list = []
 
-    for i in range(n_membranes):
-
+    for _ in range(n_membranes):
         membranes.append(Membrane(A, area, tds))
-
         k_in_list.append(pipe_k(d_in, L_in))
         k_out_list.append(pipe_k(d_out, L_out))
 
-    result = solve_parallel_system(
+    return solve_parallel_system(
         n=n_membranes,
         p_feed=p_feed,
         p_perm=p_perm,
@@ -36,5 +35,3 @@ def berechne_parallel(
         k_out_list=k_out_list,
         target_recovery=target_recovery
     )
-
-    return result
