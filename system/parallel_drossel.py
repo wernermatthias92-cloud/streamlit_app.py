370064519836c410ff52bfbf1e6df5f34192efa6
wernermatthias92-cloud <werner.matthias.92@gmail.com>
2026-04-28 10:12:34 +0200

    Update parallel_drossel.py

--- system/parallel_drossel.py
+++ system/parallel_drossel.py
@@ -28,13 +28,16 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
     tcf_real = berechne_tcf(temp)
     tcf_salz = berechne_tcf_salz(temp)
     a_wert = berechne_a_wert(m_test_flow, m_flaeche, m_test_druck, m_test_tds)
-    salzdurchgang_basis = 1.0 - m_rueckhalt
-
+    
+    salz_durchgang_nominal = 1.0 - m_rueckhalt
     if trocken_modus:
         a_wert *= 1.15
-        salzdurchgang_basis = 1.0 - max(0.0, (m_rueckhalt - 0.025))
+        salz_durchgang_nominal = 1.0 - max(0.0, (m_rueckhalt - 0.025))
 
-    salzdurchgang_real = salzdurchgang_basis * tcf_salz
+    # NEU: Punkt 6 - Selektive Ionen-Rückhaltung
+    salzdurchgang_real_nominal = salz_durchgang_nominal * tcf_salz
+    pass_hard = salzdurchgang_real_nominal * 0.15  # 80% des Salzes (Calcium etc.) blockiert extrem stark
+    pass_light = salzdurchgang_real_nominal * 1.5  # 20% des Salzes (Na, CO2) rutschen leichter durch
     
     def calc_dp(flow_lh, cfg):
         if flow_lh <= 0 or cfg.get('l', 0) <= 0: return 0.0
@@ -43,7 +46,6 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
         return (r_val * q_ms**2) / 100000.0
             
     q_min = 1.0
-    # KORREKTUR: Abfrage auf das neue Stichwort "Manometer"
     q_max_search = 30000.0 if pumpen_modus == "Manometer" else min(30000.0, q_max * 0.99) 
     
     c_d = 0.71
@@ -61,7 +63,6 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
         for bisection_it in range(60): 
             q_feed_guess = (q_min_bound + q_max_bound) / 2.0
             
-            # KORREKTUR: Abfrage auf das neue Stichwort "Manometer"
             if pumpen_modus == "Manometer":
                 p_aktuell = p_fix
                 p_verlust_saug = 0.0
@@ -92,7 +93,10 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
                 
                 p_local = p_split - p_verlust_feed
                 flow_local = f_in
-                tds_local = tds_feed
+                
+                # NEU: Aufteilung der lokalen Konzentrationen in harte und leichte Ionen
+                tds_hard_local = tds_feed * 0.80
+                tds_light_local = tds_feed * 0.20
                 
                 q_p_sum_branch = 0
                 salzfracht_sum_branch = 0
@@ -105,16 +109,28 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
                     q_p_seg = flow_local * 0.1 
                     for _ in range(5):
                         q_c_seg = max(0.001, flow_local - q_p_seg)
-                        tds_c_temp = ((flow_local * tds_local) - (q_p_seg * (tds_local * salzdurchgang_real))) / q_c_seg if q_c_seg > 0 else tds_local
-                        tds_avg = (tds_local + tds_c_temp) / 2.0
+                        
+                        # Lokale Konzentrierung getrennt für beide Ionengruppen
+                        tds_hard_c_temp = ((flow_local * tds_hard_local) - (q_p_seg * (tds_hard_local * pass_hard))) / q_c_seg if q_c_seg > 0 else tds_hard_local
+                        tds_light_c_temp = ((flow_local * tds_light_local) - (q_p_seg * (tds_light_local * pass_light))) / q_c_seg if q_c_seg > 0 else tds_light_local
+                        
+                        tds_hard_avg = (tds_hard_local + tds_hard_c_temp) / 2.0
+                        tds_light_avg = (tds_light_local + tds_light_c_temp) / 2.0
                         
                         cp_factor = berechne_cp_faktor(q_p_seg, flow_local, q_c_seg, temp, m_flaeche, area_seg)
-                        tds_wall = min(tds_avg * cp_factor, 150000.0)
-                        tds_p_target = tds_wall * salzdurchgang_real
+                        
+                        tds_hard_wall = min(tds_hard_avg * cp_factor, 150000.0)
+                        tds_light_wall = min(tds_light_avg * cp_factor, 150000.0)
+                        
+                        tds_p_target_hard = tds_hard_wall * pass_hard
+                        tds_p_target_light = tds_light_wall * pass_light
+                        tds_p_target = tds_p_target_hard + tds_p_target_light
                         
                         p_verlust_spacer_j = berechne_spacer_dp_segment(flow_local, q_c_seg, temp, n_seg)
                         p_eff_mitte = p_local - (p_verlust_spacer_j / 2)
-                        pi_wall = berechne_osmotischen_druck(tds_wall, temp)
+                        
+                        # Osmotischer Druck aus der Summe beider Salze
+                        pi_wall = berechne_osmotischen_druck(tds_hard_wall + tds_light_wall, temp)
                         
                         ndp = max(0.0, p_eff_mitte - pi_wall - p_back_total)
                         q_p_target_j = area_seg * a_wert * ndp * tcf_real
@@ -130,7 +146,10 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
                     if flow_local <= 0.001: 
                         flow_local = 0.001
                         break
-                    tds_local = ((flow_local + q_p_seg) * tds_local - q_p_seg * tds_p_target) / flow_local
+                    
+                    # Update der lokalen Salzmassen für das nächste Segment
+                    tds_hard_local = ((flow_local + q_p_seg) * tds_hard_local - q_p_seg * tds_p_target_hard) / flow_local
+                    tds_light_local = ((flow_local + q_p_seg) * tds_light_local - q_p_seg * tds_p_target_light) / flow_local
 
                 q_c_total_calc += flow_local
                 q_p_total_calc += q_p_sum_branch
@@ -145,7 +164,9 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
                 r_eff = p_drop_branch / (q_ms_f_in**2) if q_ms_f_in > 0 else 1e9
                 r_eff_list.append(r_eff)
                 
+                tds_local_total = tds_hard_local + tds_light_local
                 permeate_tds_branch = salzfracht_sum_branch / q_p_sum_branch if q_p_sum_branch > 0 else 0
+                
                 membran_daten_temp.append({
                     "Membran": membran_namen[i],
                     "Eingangsdruck (bar)": round(p_split - p_verlust_feed, 2),
@@ -155,10 +176,10 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
                     "Konzentrat (l/h)": round(flow_local, 1),
                     "Feed TDS (ppm)": round(tds_feed, 0),
                     "Permeat TDS (ppm)": round(permeate_tds_branch, 1),
-                    "Konz. TDS (ppm)": round(tds_local, 0),
+                    "Konz. TDS (ppm)": round(tds_local_total, 0),
                     "Feed µS/cm": round(tds_feed / 0.6, 0),
                     "Permeat µS/cm": round(permeate_tds_branch / 0.6, 1),
-                    "Konz. µS/cm": round(tds_local / 0.6, 0)
+                    "Konz. µS/cm": round(tds_local_total / 0.6, 0)
                 })
 
             total_permeat_guess = q_p_total_calc
@@ -196,7 +217,6 @@ def simuliere_parallel_drossel(hydraulik, drossel_vorgabe_mm, m_flaeche, m_test_
         "max_spacer_dp": max_spacer_dp,
         "membran_daten": membran_daten_temp,
         "p_verlust_saug": p_verlust_saug,
-        # KORREKTUR: Abfrage auf das neue Stichwort "Manometer"
         "p_verlust_druck_haupt": p_split - p_aktuell if pumpen_modus != "Manometer" else calc_dp(q_feed_guess, hydraulik['druck_haupt']),
         "p_effektiv_start": p_split,
         "konzentrat_druck_verlauf": max(0.0, p_vor_ventil),
